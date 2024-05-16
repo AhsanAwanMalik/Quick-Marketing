09606b23f4ef71c1989c3c002f92a08945638af2
ahsan awan <ahsanmalik@swatitech.com>
2024-05-16 13:23:30 +0500

    progree bar about us

--- lib/src/ui/views/aboutUs.dart
+++ lib/src/ui/views/aboutUs.dart
@@ -1,13 +1,10 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:animated_text_lerp/animated_text_lerp.dart';
-import 'package:latlong2/latlong.dart';
 import 'package:quickmarket/src/ui/widgets/footerWidget.dart';
 import 'package:quickmarket/src/utils/config/responsive.dart';
 import 'package:quickmarket/src/utils/config/uidata.dart';
-
+import 'package:syncfusion_flutter_charts/charts.dart';
 import '../../controllers/aboutController.dart';
 import '../../utils/config/commonscafold.dart';
 
@@ -19,8 +16,6 @@ class AboutUs extends StatelessWidget {
     return CommonScafold(
       body: NotificationListener(
         onNotification: (ScrollNotification scrollInfo) {
-          // Define the threshold for triggering the function
-          // print(scrollInfo.metrics.pixels);
           if (scrollInfo.metrics.pixels > 630 &&
               scrollInfo.metrics.pixels < 700) {
             _.updateNumericValue(100, 1500, 3592, 88);
@@ -41,88 +36,6 @@ class AboutUs extends StatelessWidget {
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  // Text(
-                  //   'OUR STORY',
-                  //   style: TextStyle(
-                  //       fontSize: 40,
-                  //       color: Color.fromARGB(166, 0, 0, 0),
-                  //       fontFamily: 'Popins',
-                  //       fontWeight: FontWeight.w800,
-                  //       letterSpacing: 0),
-                  // ).marginOnly(top: 65),
-                  // Divider(
-                  //   thickness: 1,
-                  //   color: Colors.black,
-                  //   endIndent: Get.width / 1.6,
-                  // ).marginSymmetric(vertical: 30),
-                  // Text(
-                  //   'We are here to provide you with well-planned and executable strategies for your brand to develop and grow. We aim at taking your brand to the next level of \n accomplishment via organized series of steps including:',
-                  //   style:
-                  //       TextStyle(fontWeight: FontWeight.normal, fontSize: 16),
-                  // ),
-                  // RichText(
-                  //   text: TextSpan(
-                  //     text:
-                  //         '• Designing innovative productions and services\n• Effective, informative and, attractive advertising\n• Making the best use of the digital platform\n',
-                  //     style: TextStyle(
-                  //         fontWeight: FontWeight.normal, fontSize: 15),
-                  //     children: const <TextSpan>[
-                  //       TextSpan(
-                  //         text: '• Social media promotions\n',
-                  //         style: TextStyle(
-                  //             fontSize: 15,
-                  //             fontWeight: FontWeight.w800,
-                  //             fontFamily: 'Popins',
-                  //             color: Color.fromARGB(255, 226, 38, 88)),
-                  //       ),
-                  //       TextSpan(
-                  //         text:
-                  //             '• Effective solutions for management issues.\n• Trust building between you and your customers.',
-                  //         style: TextStyle(
-                  //             fontWeight: FontWeight.normal, fontSize: 15),
-                  //       ),
-                  //     ],
-                  //   ),
-                  // ).marginSymmetric(vertical: 20),
-                  // Text(
-                  //     'We are new but youthful, enthusiastic, and full of unique ideas to help you achieve your goals. We are indeed best in our services and trusted by our clients. Your\n imagination and our expression together can create wonders. Well make sure to devise the best execution of your perception that will be productive, economically\n friendly, and fruit-bearing. Your customers will not only love your work but also be loyal to you for it.',
-                  //     style: TextStyle(
-                  //         fontWeight: FontWeight.normal, fontSize: 16)),
-
-                  // Text(
-                  //   'OUR CLIENTS',
-                  //   style: TextStyle(
-                  //       fontSize: 40,
-                  //       color: Color.fromARGB(166, 0, 0, 0),
-                  //       fontFamily: 'Popins',
-                  //       fontWeight: FontWeight.w800,
-                  //       letterSpacing: 0),
-                  // ).marginOnly(top: 65),
-                  // Container(
-                  //   height: 500,
-                  //   child: GridView.builder(
-                  //     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  //       crossAxisCount: !Responsive.isDesktopContext(context)
-                  //           ? 3
-                  //           : 5, // Number of columns
-                  //       crossAxisSpacing: 8.0, // Spacing between columns
-                  //       mainAxisSpacing: 8.0, // Spacing between rows
-                  //     ),
-                  //     itemCount: 10, // Number of items in the grid
-                  //     itemBuilder: (BuildContext context, int index) {
-                  //       return Container(
-                  //         decoration: BoxDecoration(
-                  //             borderRadius: BorderRadius.circular(20),
-                  //             border: Border.all(color: Colors.grey)),
-                  //         // color: Colors.blueAccent,
-                  //         child: Text(
-                  //           'Item $index',
-                  //           style: TextStyle(color: Colors.white),
-                  //         ),
-                  //       );
-                  //     },
-                  //   ),
-                  // ).marginOnly(top: 20),
                   SizedBox(
                     height: Get.height / 20,
                   ),
@@ -213,10 +126,92 @@ class AboutUs extends StatelessWidget {
                       ],
                     ),
                   ),
-                  CircularProgressIndicator(
-                    value: 70.0,
-                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      SizedBox(
+                        height: 156.0, // Fixed size for the chart
+                        width: 156.0, // Fixed size for the chart
+                        child: SfCircularChart(
+                          
+                          margin: const EdgeInsets.symmetric(vertical: 10),
+                          series: <CircularSeries>[
+                            DoughnutSeries<ChartData, String>(
+                              dataSource: getChartData(),
 
+                              xValueMapper: (ChartData data, _) =>
+                                  data.category,
+                              yValueMapper: (ChartData data, _) => data.value,
+                              innerRadius: '65%',
+                              radius: '100%',
+                              dataLabelSettings: DataLabelSettings(
+                                  isVisible: true), // Hide labels
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 156.0, // Fixed size for the chart
+                        width: 156.0, // Fixed size for the chart
+                        child: SfCircularChart(
+                          margin: const EdgeInsets.symmetric(vertical: 10),
+                          series: <CircularSeries>[
+                            DoughnutSeries<ChartData, String>(
+                              dataSource: getChartData1(),
+
+                              xValueMapper: (ChartData data, _) =>
+                                  data.category,
+                              yValueMapper: (ChartData data, _) => data.value,
+                              innerRadius: '65%',
+                              radius: '100%',
+                              dataLabelSettings: DataLabelSettings(
+                                  isVisible: true), // Hide labels
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 156.0, // Fixed size for the chart
+                        width: 156.0, // Fixed size for the chart
+                        child: SfCircularChart(
+                          margin: const EdgeInsets.symmetric(vertical: 10),
+                          series: <CircularSeries>[
+                            DoughnutSeries<ChartData, String>(
+                              dataSource: getChartData2(),
+
+                              xValueMapper: (ChartData data, _) =>
+                                  data.category,
+                              yValueMapper: (ChartData data, _) => data.value,
+                              innerRadius: '65%',
+                              radius: '100%',
+                              dataLabelSettings: DataLabelSettings(
+                                  isVisible: true), // Hide labels
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 156.0, // Fixed size for the chart
+                        width: 156.0, // Fixed size for the chart
+                        child: SfCircularChart(
+                          margin: const EdgeInsets.symmetric(vertical: 10),
+                          series: <CircularSeries>[
+                            DoughnutSeries<ChartData, String>(
+                              dataSource: getChartData3(),
+
+                              xValueMapper: (ChartData data, _) =>
+                                  data.category,
+                              yValueMapper: (ChartData data, _) => data.value,
+                              innerRadius: '65%',
+                              radius: '100%',
+                              dataLabelSettings: DataLabelSettings(
+                                  isVisible: true), // Hide labels
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                   Container(
                     width:
                         Responsive.isMobileContext(context) ? Get.width : null,
@@ -398,4 +393,40 @@ class AboutUs extends StatelessWidget {
       bottomNav: false,
     );
   }
+}
+
+class ChartData {
+  final String category;
+  final double value;
+  final Color color;
+
+  ChartData(this.category, this.value, this.color);
+}
+
+List<ChartData> getChartData() {
+  return [
+    ChartData('Completed', 40, UIDataColors.blueColor), // 70% completed
+    ChartData('Remaining', 60, UIDataColors.commonColor), // 30% remaining
+  ];
+}
+
+List<ChartData> getChartData1() {
+  return [
+    ChartData('Completed', 90, UIDataColors.blueColor), // 70% completed
+    ChartData('Remaining', 10, UIDataColors.commonColor), // 30% remaining
+  ];
+}
+
+List<ChartData> getChartData2() {
+  return [
+    ChartData('Completed', 60, UIDataColors.blueColor), // 70% completed
+    ChartData('Remaining', 40, UIDataColors.commonColor), // 30% remaining
+  ];
+}
+
+List<ChartData> getChartData3() {
+  return [
+    ChartData('Completed', 80, UIDataColors.blueColor), // 70% completed
+    ChartData('Remaining', 20, UIDataColors.commonColor), // 30% remaining
+  ];
 }
